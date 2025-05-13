3710b54c50c4431c3a8cf0f52c0d1a860ee36952
Abdelrahman S. Eltyar <as.eltyar@gmail.com>
2025-05-13 13:28:14 +0300

    إضافة ملف __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Purchase State Filter and Group By',
+    'version': '17.0.1.0',
+    'category': 'Purchase',
+    'summary': 'Add filtering and grouping by state in purchase orders',
+    'author': 'Your Name',
+    'website': 'https://github.com/aseltyar/purchase_state_filter',
+    'license': 'AGPL-3',
+    'depends': ['purchase'],
+    'data': [
+        'views/purchase_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
